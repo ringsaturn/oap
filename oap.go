98d3b6a63feb5d9ae57fecda95ac819a8986a020
ringsaturn <ringsaturn.me@gmail.com>
2022-04-13 01:19:08 +0800

    init support for bool/int/float32/float64

--- oap.go
+++ oap.go
@@ -2,6 +2,8 @@ package oap
 
 import (
 	"reflect"
+	"strconv"
+	"strings"
 
 	"github.com/philchia/agollo/v4"
 )
@@ -10,14 +12,63 @@ func Decode(ptr interface{}, client agollo.Client, opts ...agollo.OpOption) erro
 	v := reflect.ValueOf(ptr).Elem()
 	for i := 0; i < v.NumField(); i++ {
 		structField := v.Type().Field(i)
-		// Ignore not support type
-		if structField.Type.Kind() != reflect.String {
-			continue
-		}
 		tag := structField.Tag
 		apolloKey := tag.Get("apollo")
+		if apolloKey == "" {
+			// Ignore empty key
+			continue
+		}
 		confV := client.GetString(apolloKey, opts...)
-		v.FieldByName(structField.Name).Set(reflect.ValueOf(confV).Convert(structField.Type))
+		if confV == "" {
+			continue
+		}
+
+		var valueToSet reflect.Value
+		switch structField.Type.Kind() {
+		case reflect.String:
+			valueToSet = reflect.ValueOf(confV)
+		case reflect.Bool:
+			filedV := false
+			if strings.ToLower(confV) == "true" {
+				filedV = true
+			}
+			valueToSet = reflect.ValueOf(filedV)
+		case reflect.Int:
+			var filedV int
+
+			int64V, err := strconv.ParseInt(confV, 10, 64)
+			if err != nil {
+				return err
+			}
+			filedV = int(int64V)
+
+			valueToSet = reflect.ValueOf(filedV)
+		case reflect.Float32:
+			var filedV float32
+
+			float64V, err := strconv.ParseFloat(confV, 32)
+			if err != nil {
+				return err
+			}
+			filedV = float32(float64V)
+
+			valueToSet = reflect.ValueOf(filedV)
+		case reflect.Float64:
+			var filedV float64
+
+			float64V, err := strconv.ParseFloat(confV, 32)
+			if err != nil {
+				return err
+			}
+			filedV = float64V
+
+			valueToSet = reflect.ValueOf(filedV)
+		default:
+			// Not support types
+			continue
+		}
+		v.FieldByName(structField.Name).Set(valueToSet.Convert(structField.Type))
+
 	}
 	return nil
 }
