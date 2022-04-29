9aeaec76b6527b024ee6de01a2e2a5029e7947bf
ringsaturn <ringsaturn.me@gmail.com>
2022-04-29 13:56:33 +0800

    support substruct from yaml or json (#3)

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.18
 
 require github.com/philchia/agollo/v4 v4.1.4
 
-require github.com/golang/mock v1.6.0
+require (
+	github.com/golang/mock v1.6.0
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
--- oap_test.go
+++ oap_test.go
@@ -15,19 +15,32 @@ type DemoConfig struct {
 	Float64Field float32 `apollo:"float64Field"`
 	BoolField    bool    `apollo:"boolField"`
 	Substruct    struct {
-		X string `apollo:"x"`
-	}
+		X string `json:"x"`
+		Y int    `json:"y"`
+	} `apollo:"substruct,json"`
+	SubstructFromYAML struct {
+		X string `yaml:"x"`
+		Y int    `yaml:"y"`
+	} `apollo:"substructFromYAML,yaml"`
 }
+
+var testJSONText string = `{"x": "123", "y": 0}`
+var yamlText string = `
+x: "fffff"
+y: 12313212
+`
 
 func TestDo(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	client := NewMockClient(ctrl)
 
-	client.EXPECT().GetString(gomock.Eq("foo")).Return("bar")
-	client.EXPECT().GetString(gomock.Eq("hello")).Return("hello")
-	client.EXPECT().GetString(gomock.Eq("float32Field")).Return("3.14")
-	client.EXPECT().GetString(gomock.Eq("float64Field")).Return("3.14159265")
-	client.EXPECT().GetString(gomock.Eq("boolField")).Return("true")
+	client.EXPECT().GetString(gomock.Eq("foo")).Return("bar").MaxTimes(1)
+	client.EXPECT().GetString(gomock.Eq("hello")).Return("hello").MaxTimes(1)
+	client.EXPECT().GetString(gomock.Eq("float32Field")).Return("3.14").MaxTimes(1)
+	client.EXPECT().GetString(gomock.Eq("float64Field")).Return("3.14159265").MaxTimes(1)
+	client.EXPECT().GetString(gomock.Eq("boolField")).Return("true").MaxTimes(1)
+	client.EXPECT().GetString(gomock.Eq("substruct")).Return(testJSONText).MaxTimes(1)
+	client.EXPECT().GetString(gomock.Eq("substructFromYAML")).Return(yamlText).MaxTimes(1)
 
 	conf := &DemoConfig{}
 	if err := oap.Decode(conf, client, make(map[string][]agollo.OpOption)); err != nil {
@@ -49,6 +62,21 @@ func TestDo(t *testing.T) {
 
 	if !conf.BoolField {
 		t.Fatalf("BoolField should be `true` but got %v", conf.BoolField)
+	}
+
+	if &conf.Substruct == nil {
+		t.Fatal("nil")
+	}
+	if conf.Substruct.X != "123" {
+		t.Fatalf("Substruct.X should be `123` but got %v", conf.Substruct.X)
+	}
+
+	if conf.SubstructFromYAML.X != "fffff" {
+		t.Fatalf("SubstructFromYAML.X should be `'fffff'` but got %v", conf.SubstructFromYAML.X)
+	}
+
+	if conf.SubstructFromYAML.Y != 12313212 {
+		t.Fatalf("SubstructFromYAML.Y should be `'12313212'` but got %v", conf.SubstructFromYAML.Y)
 	}
 
 }
